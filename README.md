afb0dd288f5b7e771e832d38c9ee8c4067544ada
Yiğit Alp Ünal <62855714+yigitalpunal@users.noreply.github.com>
2025-06-12 00:24:13 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,44 @@
+## 📁 Task 2 – Point3D
+
+```markdown
+# Task 2 – Point3D Class in C++
+
+## Description
+
+This task involves creating a `Point3D` class that represents a point in 3D space. It uses OOP principles such as encapsulation and operator overloading.
+
+### Features:
+
+- Default and parameterized constructors
+- Getter and setter methods for x, y, z
+- Input (`>>`) and output (`<<`) operators
+- Overloaded `+` operator to add two points
+- Overloaded `+=` operator to shift a point by an integer
+- Uses `friend` functions
+
+## Files
+
+- `main.cpp`: Source code for the Point3D class and example usage
+
+## How to Compile and Run
+
+```bash
+g++ main.cpp -o point3d
+./point3d
+
+## Example Output
+
+Enter x: 1
+Enter y: 2
+Enter z: 3
+
+Point 1: (1, 2, 3)
+Point 2: (3, 4, 5)
+point1 + point2 = (4, 6, 8)
+After point1 += 4: (5, 6, 7)
+
+## Author
+
+Yiğit Alp Ünal
+📧 unalyigitalpp@gmail.com
+🌐 github.com/yigitalpunal
---
+++ Task2Project_Yigit_Alp_Unal.cpp
@@ -0,0 +1,90 @@
+﻿#include <iostream>
+using namespace std;
+
+class Point3D {
+private:
+    int x, y, z;
+
+public:
+    // Default constructor
+    Point3D() {
+        x = 0;
+        y = 0;
+        z = 0;
+    }
+
+    // Initializing constructor
+    Point3D(int xVal, int yVal, int zVal) {
+        x = xVal;
+        y = yVal;
+        z = zVal;
+    }
+
+    // Set functions
+    void setX(int val) { x = val; }
+    void setY(int val) { y = val; }
+    void setZ(int val) { z = val; }
+
+    // Get functions
+    int getX() const { return x; }
+    int getY() const { return y; }
+    int getZ() const { return z; }
+
+    // Friend function to input point
+    friend istream& operator>>(istream& in, Point3D& p);
+
+    // Friend function to output point
+    friend ostream& operator<<(ostream& out, const Point3D& p);
+
+    // Friend function to add two points
+    friend Point3D operator+(const Point3D& p1, const Point3D& p2);
+
+    // Overloaded += operator
+    Point3D& operator+=(int value) {
+        x += value;
+        y += value;
+        z += value;
+        return *this;
+    }
+};
+
+// Input operator >>
+istream& operator>>(istream& in, Point3D& p) {
+    cout << "Enter x: ";
+    in >> p.x;
+    cout << "Enter y: ";
+    in >> p.y;
+    cout << "Enter z: ";
+    in >> p.z;
+    return in;
+}
+
+// Output operator <<
+ostream& operator<<(ostream& out, const Point3D& p) {
+    out << "(" << p.x << ", " << p.y << ", " << p.z << ")";
+    return out;
+}
+
+// Add two points
+Point3D operator+(const Point3D& p1, const Point3D& p2) {
+    return Point3D(p1.x + p2.x, p1.y + p2.y, p1.z + p2.z);
+}
+
+// Main function
+int main() {
+    Point3D point1, point2(1, 2, 3);
+
+    cout << "Enter coordinates for point1:\n";
+    cin >> point1;
+
+    cout << "\nPoint 1: " << point1 << endl;
+    cout << "Point 2: " << point2 << endl;
+
+    Point3D sum = point1 + point2;
+    cout << "point1 + point2 = " << sum << endl;
+
+    point1 += 4;
+    cout << "After point1 += 4: " << point1 << endl;
+
+    return 0;
+}
